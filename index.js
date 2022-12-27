2d4a19593f1ebd630154917c1cf1c424c895cc5f
Nikolai Mora Lozano <nikolai.moralozano@tecomon.com>
2022-12-27 23:49:48 +0100

    better UI, connect with new APIs, add priority

--- index.js
+++ index.js
@@ -5,67 +5,14 @@ window.addEventListener("load", () => {
 const ToDo = {
     toDoArray: [],
     addToDoInputActive: false,
-    loggedIn: null,
-    sessionCookie: null,
 
     async init(){
-        let loginInput = document.getElementById("passwordInput");
-        let login = document.getElementById("login");
-        let cookieString = document.cookie.replace(/\s/g, '');
-        let logoutButton = document.getElementById("logoutButton");
-
-        for(let cookie of cookieString.split(';')) {
-            let cookieSplit = cookie.split("=");
-            
-            if(cookieSplit[0] == "session") {
-                this.sessionCookie = cookieSplit[1];
-            }
-        }
-
-        logoutButton.addEventListener("click", (evt) => {
-            login.style.display = "flex";
-            loginInput.value = "";
-            loginInput.focus();
-            evt.target.style.visibility = "hidden";
-            this.loggedIn = false;
-
-            document.cookie = "session=";
-            for(let div of document.getElementById("ListContainer").getElementsByTagName("div")) {
-                if(!div.classList.contains("ListTitle")) {
-                    div.remove();
-                }
-            }
-        });
-
-        loginInput.onkeydown = (evt) => {
-            if(evt.key == "Enter") {
-                if(evt.target.value == "123456") {  //for Authentication vulnerability
-                    login.style.display = "none";
-                    document.cookie = "session=12345";
-
-                    logoutButton.style.visibility = "visible";
-                    this.loggedIn = true;
-                    this.initControls();
-                } else {
-                    evt.target.style.outline = "3px solid red";
-                }
-            }
-        }
-
-        if(this.sessionCookie != "12345") {
-            loginInput.focus();
-            this.loggedIn = false;
-        } else {
-            login.style.display = "none";
-            logoutButton.style.visibility = "visible";
-            this.initControls();
-            this.loggedIn = true;
-        }
+        this.initControls();
     },
 
     async initControls() {
         document.getElementById("ListAddItemContainer").addEventListener("click", () => {
-            if(this.loggedIn == true) this.addToDo();
+            this.addToDo();
         });
 
         await fetch("http://127.0.0.1:8080/todos/")
@@ -76,19 +23,7 @@ const ToDo = {
             console.log(data);
             
             for(let todo of data){
-                let div = document.createElement("div");
-                div.classList.add("ListItemContainer");
-                div.innerHTML = todo.todo;  //for XSS vulnerability
-
-                let svg = document.createElement("svg");
-                svg.classList.add("checkToDoIcon");
-                svg.innerHTML = '<svg width="60" height="60" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" > <path d="M10.5858 13.4142L7.75735 10.5858L6.34314 12L10.5858 16.2427L17.6568 9.1716L16.2426 7.75739L10.5858 13.4142Z" fill="#06c258" /> </svg>'
-                svg.addEventListener("click", () => {
-                    if(this.loggedIn == true) this.deleteToDo(todo.todo, svg.parentElement)
-                });
-
-                div.appendChild(svg);
-                document.getElementById("ListContainer").appendChild(div);
+                this.createToDoDOM(null, todo.todo, todo.priority);
             }
         });
     },
@@ -100,31 +35,31 @@ const ToDo = {
 
             let input = document.createElement("input");
             input.classList.add("ListAddItemInput");
+
+            let inputPrio = document.createElement("input");
+            inputPrio.type = "number";
+            inputPrio.min = "0";
+            inputPrio.classList.add("ListAddPrioInput");
+
             input.onkeydown = (evt) => {
                 if(evt.key == "Enter") {
-                    if(input.value != "") {
-                        let des = input.value;
-
+                    if(inputPrio.value != "" && input.value != "") {
                         evt.target.blur();
-                        div.innerHTML = des;  //for XSS vulnerability
-                        div.classList.remove("ListNewItemContainer");
-                        div.style.outline = "none";
-
-                        document.getElementById("ListAddItemMessage").style.display = "none";
-
-                        let svg = document.createElement("svg");
-                        svg.classList.add("checkToDoIcon");
-                        svg.innerHTML = '<svg width="60" height="60" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" > <path d="M10.5858 13.4142L7.75735 10.5858L6.34314 12L10.5858 16.2427L17.6568 9.1716L16.2426 7.75739L10.5858 13.4142Z" fill="#06c258" /> </svg>'
-
-                        svg.addEventListener("click", () => this.deleteToDo(des, svg.parentElement));
-
-                        div.appendChild(svg);
-
-                        this.saveToDo(des);
+                        this.createToDoDOM(div, input.value, inputPrio.value);
                     }
                 }
             }
+            inputPrio.onkeydown = (evt) => {
+                if(evt.key == "Enter") {
+                    if(inputPrio.value != "" && input.value != "") {
+                        evt.target.blur();
+                        this.createToDoDOM(div, input.value, inputPrio.value);
+                    }
+                }
+            }
+
             div.appendChild(input);
+            div.appendChild(inputPrio);
 
             this.addToDoInputActive = true;
             document.getElementById("ListContainer").appendChild(div);
@@ -135,24 +70,67 @@ const ToDo = {
         }
     },
 
-    async saveToDo(des) {
+    createToDoDOM(div, value, prio) {
+        let newContainer = null;
+
+        if(div != null) {
+            div.classList.remove("ListNewItemContainer");
+
+            newContainer = false;
+        } else {
+            div = document.createElement("div");
+            div.classList.add("ListItemContainer");
+
+            newContainer = true;
+        }
+
+        let prioDiv = document.createElement("div");
+        prioDiv.classList.add("prio");
+        prioDiv.innerText = prio;
+
+        div.innerHTML = value;  //for XSS vulnerability
+        div.style.outline = "none";
+
+        document.getElementById("ListAddItemMessage").style.display = "none";
+
+        let svg = document.createElement("svg");
+        svg.classList.add("checkToDoIcon");
+        svg.innerHTML = '<svg width="60" height="60" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" > <path d="M10.5858 13.4142L7.75735 10.5858L6.34314 12L10.5858 16.2427L17.6568 9.1716L16.2426 7.75739L10.5858 13.4142Z" fill="#06c258" /> </svg>'
+
+        svg.addEventListener("click", () => this.deleteToDo(value, svg.parentElement, prio));
+
+        div.appendChild(prioDiv);
+        div.appendChild(svg);
+
+        this.saveToDo(value, prio);
+
+        if(newContainer == true) document.getElementById("ListContainer").appendChild(div);
+    },
+
+    async saveToDo(des, prio) {
         this.addToDoInputActive = false;
 
-        await fetch("http://127.0.0.1:8080/todos/?name="+des, {
+        let json = { "todo": des, "priority": prio };
+
+        await fetch("http://127.0.0.1:8080/todos/", {
             method: "POST",
-            headers: {'Content-Type': 'application/json'}
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(json)
         })
         .then(res => {
             console.log(res);
         });
     },
 
-    async deleteToDo(des, item) {
+    async deleteToDo(des, item, prio) {
         item.remove();
 
-        await fetch("http://127.0.0.1:8080/todos/?name="+des, {
+        let json = { "todo": des, "priority": prio };
+
+        await fetch("http://127.0.0.1:8080/todos/", {
             method: "DELETE",
-            headers: {'Content-Type': 'application/json'}
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(json)
         })
         .then(res => {
             console.log(res);
