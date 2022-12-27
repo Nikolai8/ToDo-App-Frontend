b684e1d337ceeef2256b0ee5ec683bcd8cc21f8c
Nikolai Mora Lozano <nikolai.moralozano@tecomon.com>
2022-12-27 23:53:31 +0100

    clean code

--- index.js
+++ index.js
@@ -88,14 +88,14 @@ const ToDo = {
         prioDiv.classList.add("prio");
         prioDiv.innerText = prio;
 
-        div.innerHTML = value;  //for XSS vulnerability
+        div.innerText = value;
         div.style.outline = "none";
 
         document.getElementById("ListAddItemMessage").style.display = "none";
 
         let svg = document.createElement("svg");
         svg.classList.add("checkToDoIcon");
-        svg.innerHTML = '<svg width="60" height="60" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" > <path d="M10.5858 13.4142L7.75735 10.5858L6.34314 12L10.5858 16.2427L17.6568 9.1716L16.2426 7.75739L10.5858 13.4142Z" fill="#06c258" /> </svg>'
+        svg.innerText = '<svg width="60" height="60" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" > <path d="M10.5858 13.4142L7.75735 10.5858L6.34314 12L10.5858 16.2427L17.6568 9.1716L16.2426 7.75739L10.5858 13.4142Z" fill="#06c258" /> </svg>'
 
         svg.addEventListener("click", () => this.deleteToDo(value, svg.parentElement, prio));
 
